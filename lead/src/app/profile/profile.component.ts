8cc4af285a7499a288ebb6842d39f0f7890dbdbf
rahulaitrich <rahul.aitrich@gmail.com>
2024-02-17 16:06:11 +0530

    editing in responsiveness

--- lead/src/app/profile/profile.component.ts
+++ lead/src/app/profile/profile.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  companyName: string = "Aitrich Technologies";
+  originalCompanyName: string = ""; // Variable to store original company name
+  isEditingName: boolean = false;
 
+  toggleEditName() {
+    if (!this.isEditingName) {
+      // Store the original company name before editing
+      this.originalCompanyName = this.companyName;
+    } else {
+      // If editing is canceled, restore the original company name
+      // If the original company name is not empty, restore it
+      // Otherwise, reset to default value
+      this.companyName = this.originalCompanyName !== '' ? this.originalCompanyName : 'Aitrich Technologies';
+    }
+    this.isEditingName = !this.isEditingName; // Toggle editing mode
+  }
 }
